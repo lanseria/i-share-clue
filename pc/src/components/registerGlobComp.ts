00169877c805beaad7722a5a59961d35b3436346
lanseria <zhangchao564265135@hotmail.com>
2022-01-05 16:32:22 +0800

    添加开源视频播放器

--- pc/src/components/registerGlobComp.ts
+++ pc/src/components/registerGlobComp.ts
@@ -1,13 +1,16 @@
-import type { App } from "vue";
+import type { App } from 'vue';
+import vue3videoPlay from 'vue3-video-play'; // 引入组件
+import 'vue3-video-play/dist/style.css'; // 引入css
 // import { Icon } from './Icon';
-import ImpPageContainer from "./global/ImpPageContainer.vue";
-import ImpImage from "./global/ImpImage.vue";
-import ImpModal from "./global/ImpModal.vue";
+import ImpPageContainer from './global/ImpPageContainer.vue';
+import ImpImage from './global/ImpImage.vue';
+import ImpModal from './global/ImpModal.vue';
 
 const compList = [ImpPageContainer, ImpImage, ImpModal];
 
 export function registerGlobComp(app: App) {
-  compList.forEach(comp => {
+  app.use(vue3videoPlay);
+  compList.forEach((comp) => {
     app.component(comp.name || comp.displayName, comp);
   });
 }
--- pc/src/views/pages/transfy/UploadVideo/index.vue
+++ pc/src/views/pages/transfy/UploadVideo/index.vue
@@ -1,26 +1,28 @@
 <template>
-  <n-upload ref="UploadRef" :max="1" accept=".mkv,.mp4,.flv,.avi," :customRequest="customRequest" :on-remove="handleRemove">
-    <n-upload-dragger v-show="!showVideo">
-      <div style="margin-bottom: 12px">
-        <n-icon size="48" :depth="3">
-          <archive-icon />
-        </n-icon>
-      </div>
-      <n-text style="font-size: 16px">点击或者拖动文件到该区域来上传</n-text>
-      <n-p depth="3" style="margin: 8px 0 0 0">请不要上传敏感数据，比如你的银行卡号和密码，信用卡号有效期和安全码</n-p>
-    </n-upload-dragger>
-    <!-- <video v-else controls autoplay name="media">
-      <source :src="uploadUrl" type="video/mp4" />
-    </video> -->
-    <video v-show="showVideo" width="480" height="240" controls>
-      <source :src="uploadUrl" type="video/mp4" />
-      您的浏览器不支持 HTML5 video 标签。
-    </video>
+  <n-upload abstract ref="UploadRef" :max="1" :accept="accept" :customRequest="customRequest" :on-remove="handleRemove">
+    <div>
+      <vue3VideoPlay v-show="showVideo" v-bind="options" />
+      <n-upload-trigger #="{ handleClick }" abstract>
+        <n-upload-dragger v-show="!showVideo" @click="handleClick">
+          <div style="margin-bottom: 12px">
+            <n-icon size="48" :depth="3">
+              <archive-icon />
+            </n-icon>
+          </div>
+          <n-text style="font-size: 16px">点击或者拖动文件到该区域来上传</n-text>
+          <n-p depth="3" style="margin: 8px 0 0 0">请不要上传敏感数据，支持 {{ accept }}</n-p>
+        </n-upload-dragger>
+      </n-upload-trigger>
+
+      <n-card v-show="showVideo" embedded :bordered="false" style="margin-top: 12px">
+        <n-upload-file-list />
+      </n-card>
+    </div>
   </n-upload>
 </template>
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
-import { NUpload, NUploadDragger, NIcon, NP, NText } from 'naive-ui';
+import { computed, defineComponent, reactive, ref } from 'vue';
+import { NUpload, NUploadDragger, NUploadFileList, NUploadTrigger, NIcon, NP, NText, NCard } from 'naive-ui';
 import { Archive as ArchiveIcon } from '@vicons/ionicons5';
 import { uploadFileReq } from '/@/api/File';
 import { CustomRequest } from 'naive-ui/lib/upload/src/interface';
@@ -34,6 +36,9 @@ export default defineComponent({
     NP,
     NText,
     ArchiveIcon,
+    NUploadFileList,
+    NUploadTrigger,
+    NCard,
   },
   props: {
     url: {
@@ -44,22 +49,36 @@ export default defineComponent({
   setup(props, { emit }) {
     const uploadUrl = useVModel(props, 'url', emit);
     const UploadRef = ref();
+    const accept = ref('.mkv,.mp4,.flv,.avi,');
     const showVideo = ref(false);
+    const options = computed(() => {
+      return {
+        width: '480px', //播放器高度
+        height: '240px', //播放器高度
+        color: '#409eff', //主题色
+        currentTime: 0,
+        speed: false, //关闭进度条拖动
+        title: '', //视频名称
+        src: uploadUrl.value, //视频源
+      };
+    });
     const customRequest: CustomRequest = ({ file, withCredentials, onFinish, onError, onProgress }) => {
       uploadFileReq(file, withCredentials, onProgress)
         .then(({ payload }: any) => {
-          console.log(payload);
-          uploadUrl.value = 'http://' + payload.url;
           onFinish();
-          handleFinish();
+          setTimeout(() => {
+            handleFinish(payload);
+          }, 1000);
         })
         .catch((error) => {
           console.log(error);
           onError();
         });
     };
-    const handleFinish = () => {
+    const handleFinish = (payload: any) => {
       showVideo.value = true;
+      // console.log(payload);
+      uploadUrl.value = 'http://' + payload.url;
     };
     const handleRemove = () => {
       console.log('remove');
@@ -69,8 +88,11 @@ export default defineComponent({
     return {
       UploadRef,
       // ref
+      accept,
       showVideo,
       uploadUrl,
+      // computed
+      options,
       // method
       customRequest,
       handleRemove,
